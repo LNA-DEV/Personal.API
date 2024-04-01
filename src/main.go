f33937e27f85a5b8d21004140f5f2141cf988434
Lukas Nagel <lukas@lna-dev.net>
2024-04-01 11:25:28 +0200

    🚧 Added gin

--- src/main.go
+++ src/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+    "github.com/gin-gonic/gin"	
+)
+
 func main(){
+    router := gin.Default()
+    router.GET("/autouploader/pixelfed", getUploadedItems)
+
+    router.Run("localhost:8080")
+}
+
+func getUploadedItems(c *gin.Context){
 	
 }
